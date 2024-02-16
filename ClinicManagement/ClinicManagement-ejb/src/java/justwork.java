fdd108cc846011556aabfaae5daaece79b76a7bc
Foo Zhi Sheng <105271950+zhisheng-foo@users.noreply.github.com>
2024-02-16 23:14:12 +0800

    Please work i have no apples

---
+++ ClinicManagement/ClinicManagement-ejb/src/java/justwork.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author foozh
+ */
+public class justwork {
+    
+    public static void main(String[] args) {
+        
+     System.out.println("First I was afriad and then I was petrified");   
+        
+    }
+    
+}
