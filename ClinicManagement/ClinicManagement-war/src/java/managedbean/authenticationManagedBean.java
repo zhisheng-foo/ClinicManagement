39daa52cc02507e97bbf62f942811a870e3052ee
Nicholas <kathnick01@gmail.com>
2024-03-19 13:26:26 +0800

    added source package folders

---
+++ ClinicManagement/ClinicManagement-war/src/java/managedbean/authenticationManagedBean.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author nicholas
+ */
+@Named(value = "authenticationManagedBean")
+@SessionScoped
+public class authenticationManagedBean implements Serializable {
+
+    public authenticationManagedBean() {
+    }
+    
+    public void login() {
+        
+    }
+    
+}
