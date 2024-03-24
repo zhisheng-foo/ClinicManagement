607ac43590c0a6bb4bb592d38461a5025b6b3a05
Shion Higashi <shionhigashi@gmail.com>
2024-03-24 15:05:51 +0800

    staff managed bean created

---
+++ ClinicManagement/ClinicManagement-war/src/java/managedbean/StaffManagedBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author shionhigashi
+ */
+@Named(value = "staffManagedBean")
+@Dependent
+public class StaffManagedBean {
+
+    /**
+     * Creates a new instance of StaffManagedBean
+     */
+    public StaffManagedBean() {
+    }
+    
+}
